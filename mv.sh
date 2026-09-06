5f2790d772bc50d5b9765b8ad640764c84b6b8fa
Kirutka <kirya.ivanov.2019@internet.ru>
2026-09-06 22:58:05 +0700

    Доработана функция установки Zsh

--- mv.sh
+++ mv.sh
@@ -6,22 +6,38 @@ set -o pipefail
 echo "Добро пожаловать в MS!"
 
 update_system() {
-	echo "Полное обновление системы..."
-
+	echo "Полное обновление системы"
 	apt update && apt full-upgrade -y && apt autoremove -y && apt autoclean -y
-
 	echo "Система обновлена"
 }
 
 install_Zsh_OMZ_plugins() {
-	echo ""
+	if ! command -v zsh &>/dev/null; then
+		sudo apt update && sudo apt install -y zsh
+		echo "Zsh установлен"
+	else 
+		echo "Zsh уже установлен"
+	fi
+
+	if [ -d "$HOME/.oh-my-zsh" ]; then
+		echo "Oh My Zsh уже установлен"
+	else 
+		if ! command -v curl &>/dev/null && ! command -v wget &>/dev/null; then
+		echo "Устанавливаю curl"
+		sudo apt install -y curl
+	fi
+	if command -v curl &>/dev/null; then
+            sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+			echo "Oh My Zsh установлен"
+	fi
+fi
 }
 
 
 options=(
 	"1 - Полное обновление системы"
 	"2 - Установка базовых утилит (нет)"
-	"3 - Установка Zsh + Oh My Zsh (нет)"
+	"3 - Установка Zsh + Oh My Zsh"
 	"0 - Выход"
 	)
 
@@ -33,6 +49,10 @@ while true; do
 		case $REPLY in
 			1)
 				update_system
+				break
+				;;
+			3) 
+				install_Zsh_OMZ_plugins
 				break
 				;;
 
